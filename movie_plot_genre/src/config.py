b8a434ed007183450eb614872d7dbbce096957d7
idrishaidar <idrishaidar@gmail.com>
2022-01-14 18:25:34 +0700

    initialize movie plot genre

---
+++ movie_plot_genre/src/config.py
@@ -0,0 +1,8 @@
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+ACCUMULATION = 2
+BERT_PATH = '../input/bert_base_uncased/'
+MODEL_PATH = 'model.bin'
+TRAINING_FILE = '../Input/wiki_movie_plots_deduped.csv'
